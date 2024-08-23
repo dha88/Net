b2dc6f6d1fc9c9ce9c9f82654572e10116d68c4a
Dhanasekhar88 <152867040+dha88@users.noreply.github.com>
2024-08-23 11:18:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const faqs=document.querySelectorAll(".faq")
+
+faqs.forEach((faq)=>{
+    faq.addEventListener("click",()=>{
+        if (faq.classList.contains("active")){
+            faq.classList.remove("active")
+        }else{
+          faq.classList.add("active")
+        }
+
+    })
+})
